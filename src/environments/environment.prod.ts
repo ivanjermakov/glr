07ab8ff6fc25258ae833ec66b663e8fc5d779d0f
ivanjermakov <ivanjermakov1@gmail.com>
2022-04-14 09:50:46 +0200

    Environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://ukrvxpeqhaxdixnhqboy.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrcnZ4cGVxaGF4ZGl4bmhxYm95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk5MTQ0NTUsImV4cCI6MTk2NTQ5MDQ1NX0.eXK2-wXBwIQh8jr4ZuY0-OC0C2nN7d9R_FGC-N3yEmw',
+  s3Path: 'https://s3.eu-central-1.amazonaws.com/photos.ivnj.org/',
+  imagePathPrefix: 'photos/',
+  thumbnailPathPrefix: 'thumbnails/_',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,6 @@
+import {environment as prod} from './environment.prod'
+
 export const environment = {
+  ...prod,
   production: false,
-  supabaseUrl: 'https://ukrvxpeqhaxdixnhqboy.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrcnZ4cGVxaGF4ZGl4bmhxYm95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk5MTQ0NTUsImV4cCI6MTk2NTQ5MDQ1NX0.eXK2-wXBwIQh8jr4ZuY0-OC0C2nN7d9R_FGC-N3yEmw',
-  s3Path: 'https://s3.eu-central-1.amazonaws.com/photos.ivnj.org/',
-  imagePathPrefix: 'photos/',
-  thumbnailPathPrefix: 'thumbnails/_',
 }
